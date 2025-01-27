94c431c0070541344a41fcc33439034e028a9c1b
binarysimple17 <vred33@gmail.com>
2025-01-27 16:47:04 +0300

    final part 01

--- src/ex_03/Ex_03.java
+++ src/ex_03/Ex_03.java
@@ -1,7 +1,5 @@
 package ex_03;
 
-import java.util.Scanner;
-
 public class Ex_03 {
     public static void main(String[] args) {
         new Ex_03().run();
@@ -10,16 +8,6 @@ public class Ex_03 {
     public void run() {
 //    % — остаток от деления
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите трехзначное число: ");
-        int number = scanner.nextInt();
-
-        int digit1 = number / 100;        // Первая цифра
-        int digit2 = (number / 10) % 10;  // Вторая цифра
-        int digit3 = number % 10;         // Третья цифра
-
-        int sum = digit1 + digit2 + digit3;
-        System.out.println("Сумма цифр: " + sum);
 
     }
 }
