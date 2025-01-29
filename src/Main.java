b612cee127ae36dc99a582e8c21c99fb5e958400
binarysimple17 <vred33@gmail.com>
2025-01-29 16:31:40 +0300

    comments

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ import ex_01.Ex_01;
 import ex_02.Ex_02;
 import ex_03.Ex_03;
 import ex_04.Ex_04;
+import ex_05.Ex_05;
+import ex_06.Ex_06;
+import ex_07.Ex_07;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,5 +12,8 @@ public class Main {
         new Ex_02().run();
         new Ex_03().run();
         new Ex_04().run();
+        new Ex_05().run();
+        new Ex_06().run();
+        new Ex_07().run();
     }
 }
